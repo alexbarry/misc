71a03579b1bb4082aedaeaa0e36d003f4998a7aa
Alex Barry <alexbarry@users.noreply.github.com>
2024-04-03 10:10:55 -0700

    add a screenshot, update html title, add clarifying comments to main.js

--- emscripten_multiple_mains/main.js
+++ emscripten_multiple_mains/main.js
@@ -1,3 +1,12 @@
+// This is to simply render index.html in an electron app,
+// which crashes the renderer process and is tricky to debug.
+// Run this command to try it:
+//     ELECTRON_ENABLE_LOGGING=1 electron .
+//
+// Note that without adding `ELECTRON_ENABLE_LOGGING=1`, the
+// dev tools disconnect and there is no error, making it really
+// tricky to debug.
+
 const { app, BrowserWindow } = require('electron/main')
 
 const createWindow = () => {
